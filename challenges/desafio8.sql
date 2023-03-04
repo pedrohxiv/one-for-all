7746daecd93e6c2313ba3d1dbf75fed2d8a0e1cb
Pedro <pedrohalmeidamendonca@gmail.com>
2023-03-04 15:26:36 -0300

    Requisito 8

---
+++ challenges/desafio8.sql
@@ -0,0 +1,1 @@
+SELECT ar.artist_name AS `artista`, al.album_name AS `album` FROM SpotifyClone.artists AS ar INNER JOIN SpotifyClone.albums AS al ON ar.artist_id = al.artist_id WHERE ar.artist_name = 'Elis Regina' ORDER BY al.album_name ASC;
