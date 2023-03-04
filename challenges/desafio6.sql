b690571d911354231eb39a7ca709046cc3de377b
Pedro <pedrohalmeidamendonca@gmail.com>
2023-03-04 14:45:06 -0300

    Requisito 6

---
+++ challenges/desafio6.sql
@@ -0,0 +1,1 @@
+SELECT ROUND(MIN(pl.plan_price), 2) AS `faturamento_minimo`, ROUND(MAX(pl.plan_price), 2) AS `faturamento_maximo`, ROUND(AVG(pl.plan_price), 2) AS `faturamento_medio`, ROUND(SUM(pl.plan_price), 2) AS `faturamento_total` FROM SpotifyClone.plans AS pl JOIN SpotifyClone.users AS us ON us.plan_id = pl.plan_id;
